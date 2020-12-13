aac7ebb9a385d86156f373b0dd3269b64127a04d
Faisal Masood <fmasood@redhat.com>
2020-12-14 01:09:37 +1100

    added tranformer to alibi

--- seldon-container/CustomerChurnTransformer.py
+++ seldon-container/CustomerChurnTransformer.py
@@ -5,8 +5,8 @@ import joblib
 class CustomerChurnTransformer(object):
 
     def __init__(self):
-        self.encoder = joblib.load('CustomerChurnOrdinalEncoder.sav')
-        self.onehotencoder = joblib.load('CustomerChurnOneHotEncoder.sav')
+        self.encoder = joblib.load('CustomerChurnOrdinalEncoder.pkl')
+        self.onehotencoder = joblib.load('CustomerChurnOneHotEncoder.pkl')
 
     def transform_input(self, X, feature_names):
         X = self.encoder.transform(X)
