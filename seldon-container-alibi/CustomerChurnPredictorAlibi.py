2cda8bd5ed3386b6ebb9fd90f6c342ee30e2d93d
Faisal Masood <fmasood@redhat.com>
2020-12-14 17:05:29 +1100

    more prints

--- seldon-container-alibi/CustomerChurnPredictorAlibi.py
+++ seldon-container-alibi/CustomerChurnPredictorAlibi.py
@@ -2,6 +2,7 @@ import dill
 from alibi.explainers import AnchorTabular
 import numpy as np
 import json
+import pandas as pd
 
 class CustomerChurnPredictorAlibi(object):
 
@@ -10,16 +11,16 @@ class CustomerChurnPredictorAlibi(object):
             self.explainer = dill.load(x_f)
         
     def predict(self, X, feature_names=None,  **kwargs):
-        print("Received raw numpy array: ")
-        print(X)
         oned_X = X.flatten()
-        print("Received and Flattened:")
-        print(oned_X)
         oned_X = oned_X.astype(float)
-        print("Received and Flattened Float: ")
         print(oned_X)
-        print('about to explain')
-        explanation = self.explainer.explain(oned_X)
+        total_values = len(oned_X)
+        df = pd.DataFrame(x)
+        df = df.values.reshape(3,)
+        print(df.shape)
+        print(df.values[0])
+        print(df.values[1])
+        explanation = self.explainer.explain(df.values[1])
         print("Predicted: " + str(explanation))
         return explanation['anchor']
         # return json.dumps(explanation, cls=NumpyEncoder)
@@ -34,3 +35,10 @@ class NumpyEncoder(json.JSONEncoder):
         elif isinstance(obj, (np.ndarray,)):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
+
+x = [1.0, 2.0, 3.0]
+print(len(x))
+df = pd.DataFrame(x)
+df = df.values.reshape(3,)
+print('asdasd')
+print(df.shape)
