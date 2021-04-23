89c62c37f17dbf3fafa8ec445a4b75c7c36ad001
Faisal Masood <fmasood@redhat.com>
2021-04-23 10:23:56 +1000

    commented consumer

--- kafka-populator/load-csv.py
+++ kafka-populator/load-csv.py
@@ -3,6 +3,7 @@ import os
 import json
 from time import sleep
 from kafka import KafkaProducer, KafkaConsumer
+# import sys
 
 file_location = os.getenv('CHURN_FILE') or '../data/Customer-Churn_P2.csv'
 full_product_data = pd.read_csv(file_location)
@@ -21,14 +22,19 @@ sleep(10)
 
 print("done submitting messages")
 
-consumer2 = KafkaConsumer(
-    'data',
-    bootstrap_servers=[kafka_servers],
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    group_id='fm-group',
-    value_deserializer=lambda v: json.loads(v.decode('utf-8')))
+# consumer2 = KafkaConsumer(
+#     'data',
+#     bootstrap_servers=[kafka_servers],
+#     auto_offset_reset='earliest',
+#     enable_auto_commit=True,
+#     group_id='fm-group',
+#     value_deserializer=lambda v: json.loads(v.decode('utf-8')))
 
 
-for records in consumer2:
-    print(f"**Data Consumed: {records}")
+# for records in consumer2:
+#     print(f"**Data Consumed: {records}")
+
+# print("done consuming messages")
+
+# sys.exit(0)
+
