4a5968d5a6b8148a78df6d048c44fe536f5e9cbd
Faisal Masood <fmasood@redhat.com>
2021-08-11 18:04:38 +1000

    minor refactor

--- kafka-populator/load-csv.py
+++ kafka-populator/load-csv.py
@@ -8,14 +8,15 @@ from kafka import KafkaProducer, KafkaConsumer
 file_location = os.getenv('CHURN_FILE') or '../data/Customer-Churn_P2.csv'
 full_product_data = pd.read_csv(file_location)
 
-kafka_servers = os.getenv('KAFKA_SERVERS') or 'localhost:29092'
+kafka_servers = os.getenv('KAFKA_SERVERS') or 'localhost:9092'
 print(f"Kafka is at {kafka_servers}")
 
-producer = KafkaProducer(bootstrap_servers=kafka_servers, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+# producer = KafkaProducer(bootstrap_servers=kafka_servers, value_serializer=lambda v: json.dumps(v).encode())
+producer = KafkaProducer(bootstrap_servers=kafka_servers)
 
 for index, row in full_product_data.iterrows():
     print(row.to_json())
-    producer.send('data', row.to_json())
+    producer.send('data', value = json.dumps( json.loads(row.to_json())).encode())
 
 producer.flush()
 sleep(10)
@@ -23,7 +24,7 @@ sleep(10)
 print("done submitting messages")
 
 # consumer2 = KafkaConsumer(
-#     'data',
+#     'raw-router-data',
 #     bootstrap_servers=[kafka_servers],
 #     auto_offset_reset='earliest',
 #     enable_auto_commit=True,
